7befacb747e73847c5f96165872537257b49dcc8
Mouli Sai Deep <moulisaideep.get@gmail.com>
2026-02-14 15:09:26 +0530

    Added Test file

---
+++ src/main/java/jenkins_demo/jenkins_demo/CseRocks.java
@@ -0,0 +1,11 @@
+package jenkins_demo.jenkins_demo;
+
+public class CseRocks {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int mul(int a, int b) {
+		return a * b;
+	}
+}
---
+++ src/test/java/jenkins_demo/jenkins_demo/CseRocksTest.java
@@ -0,0 +1,19 @@
+package jenkins_demo.jenkins_demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CseRocksTest {
+	CseRocks cal = new CseRocks();
+
+	@Test
+	void test1() {
+		int result = cal.mul(2, 3);
+		assertEquals(6, result);
+	}
+}
